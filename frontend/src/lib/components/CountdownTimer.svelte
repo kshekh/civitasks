2cac6893ffc9f8eeca23cfd48cfb12fd546af665
Waleed Mumtaz <waleedthedev@gmail.com>
2023-11-19 14:47:36 +0500

    Add bg image to hero section

--- frontend/src/lib/components/CountdownTimer.svelte
+++ frontend/src/lib/components/CountdownTimer.svelte
@@ -1,107 +1,86 @@
 <script>
-  import { onMount } from 'svelte';
-  import Link from '$lib/components/Link.svelte';
+	import { onMount } from 'svelte';
+	import Link from '$lib/components/Link.svelte';
 
-  let countDownDate;
-  let now;
-  let distance;
-  let days = '00';
-  let hours = '00';
-  let minutes = '00';
-  let seconds = '00';
+	let countDownDate;
+	let now;
+	let distance;
+	let days = '00';
+	let hours = '00';
+	let minutes = '00';
+	let seconds = '00';
 
-  onMount(() => {
-    countDownDate = new Date('2024-01-09 23:59:59').getTime();
-    const interval = setInterval(() => {
-      now = new Date().getTime();
-      distance = countDownDate - now;
-      days = String(Math.floor(distance / (1000 * 60 * 60 * 24))).padStart(
-        2,
-        '0'
-      );
-      hours = String(
-        Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
-      ).padStart(2, '0');
-      minutes = String(
-        Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
-      ).padStart(2, '0');
-      seconds = String(Math.floor((distance % (1000 * 60)) / 1000)).padStart(
-        2,
-        '0'
-      );
-      if (distance < 0) {
-        clearInterval(interval);
-        days = hours = minutes = seconds = '00';
-      }
-    }, 1000);
-    return () => {
-      clearInterval(interval);
-    };
-  });
+	onMount(() => {
+		countDownDate = new Date('2024-01-09 23:59:59').getTime();
+		const interval = setInterval(() => {
+			now = new Date().getTime();
+			distance = countDownDate - now;
+			days = String(Math.floor(distance / (1000 * 60 * 60 * 24))).padStart(2, '0');
+			hours = String(Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))).padStart(
+				2,
+				'0'
+			);
+			minutes = String(Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))).padStart(2, '0');
+			seconds = String(Math.floor((distance % (1000 * 60)) / 1000)).padStart(2, '0');
+			if (distance < 0) {
+				clearInterval(interval);
+				days = hours = minutes = seconds = '00';
+			}
+		}, 1000);
+		return () => {
+			clearInterval(interval);
+		};
+	});
 </script>
 
 <section>
-  <!-- TODO: add icons -->
-  <div class="text-center mb-56 md:mb-40">
-    <div
-      class="font-nb-architekt flex items-center justify-center gap-8 md:gap-12 mb-8 md:mb-12 text-5xl uppercase text-center"
-    >
-      <!-- days -->
-      <div>
-        <p class="flex flex-col items-center">
-          {days}<br /><span class="text-base">days</span>
-        </p>
-      </div>
-      <div>
-        <img
-          class="hidden md:block -translate-y-2"
-          src="/element-timer-bar.svg"
-          alt=""
-        />
-        <span class="block md:hidden -translate-y-4">:</span>
-      </div>
-      <!-- hours -->
-      <div>
-        <p class="flex flex-col items-center">
-          {hours}<br /><span class="text-base">hours</span>
-        </p>
-      </div>
-      <div>
-        <img
-          class="hidden md:block -translate-y-2"
-          src="/element-timer-bar.svg"
-          alt=""
-        />
-        <span class="block md:hidden -translate-y-4">:</span>
-      </div>
-      <!-- minutes -->
-      <div>
-        <p class="flex flex-col items-center">
-          {minutes}<br /><span class="text-base">mins</span>
-        </p>
-      </div>
-      <div class="hidden md:block">
-        <img
-          class="hidden md:block -translate-y-2"
-          src="/element-timer-bar.svg"
-          alt=""
-        />
-        <span class="block md:hidden -translate-y-4">:</span>
-      </div>
-      <!-- seconds -->
-      <div class="hidden md:block">
-        <p class="flex flex-col items-center">
-          {seconds}<br /><span class="text-base">secs</span>
-        </p>
-      </div>
-    </div>
-    <div class="max-w-xs md:max-w-lg mx-auto">
-      <h2
-        class="font-tasa-orbiter-display font-semibold text-3xl md:text-4xl mb-8"
-      >
-        Colosseum’s next Hackathon starts on January 10, 2024.
-      </h2>
-      <Link linkTo="/signup" theme="default">Sign Up</Link>
-    </div>
-  </div>
+	<!-- TODO: add icons -->
+	<div class="text-center mb-56 md:mb-40">
+		<div
+			class="font-nb-architekt flex items-center justify-center gap-8 md:gap-12 mb-8 md:mb-12 text-5xl uppercase text-center"
+		>
+			<!-- days -->
+			<div>
+				<p class="flex flex-col items-center">
+					{days}<br /><span class="text-base">days</span>
+				</p>
+			</div>
+			<div>
+				<img class="hidden md:block -translate-y-2" src="/element-timer-bar.svg" alt="" />
+				<span class="block md:hidden -translate-y-4">:</span>
+			</div>
+			<!-- hours -->
+			<div>
+				<p class="flex flex-col items-center">
+					{hours}<br /><span class="text-base">hours</span>
+				</p>
+			</div>
+			<div>
+				<img class="hidden md:block -translate-y-2" src="/element-timer-bar.svg" alt="" />
+				<span class="block md:hidden -translate-y-4">:</span>
+			</div>
+			<!-- minutes -->
+			<div>
+				<p class="flex flex-col items-center">
+					{minutes}<br /><span class="text-base">mins</span>
+				</p>
+			</div>
+			<div class="hidden md:block">
+				<img class="hidden md:block -translate-y-2" src="/element-timer-bar.svg" alt="" />
+				<span class="block md:hidden -translate-y-4">:</span>
+			</div>
+			<!-- seconds -->
+			<div class="hidden md:block">
+				<p class="flex flex-col items-center">
+					{seconds}<br /><span class="text-base">secs</span>
+				</p>
+			</div>
+		</div>
+		<div class="max-w-xs md:max-w-lg mx-auto">
+			<h2 class="font-tasa-orbiter-display font-semibold text-3xl md:text-4xl mb-8">
+				Colosseum’s next Hackathon starts on January 10, 2024.
+			</h2>
+			<Link linkTo="/signup" theme="default">Sign Up</Link>
+		</div>
+	</div>
 </section>
--- frontend/src/lib/components/Hero.svelte
+++ frontend/src/lib/components/Hero.svelte
@@ -1,37 +1,36 @@
 <script>
-  import Link from '$lib/components/Link.svelte';
+	import Link from '$lib/components/Link.svelte';
 </script>
 
 <section>
-  <!-- TODO: add bg image -->
-  <div class="font-tasa-orbiter-display text-center px-6 pt-14 md:pt-32">
-    <div class="bg-grid bg-no-repeat bg-cover">
-      <h1 class="uppercase font-bold text-6xl md:text-9xl mb-10 md:mb-2">
-        Enter the Arena
-      </h1>
-      <div class="max-w-[938px] mx-auto text-lg mb-10">
-        <p>
-          Claim your place in the next generation of online hackathons.<br
-            class="hidden sm:block"
-          />Join the most innovative builders in crypto, and launch an
-          industry-defining company on Solana.<br class="hidden sm:block" />Win
-          and be invited to Colosseum’s accelerator, where you’ll receive
-          mentorship from successful ecosystem founders, and grow your start-up
-          with funding from Colosseum and other top investors.
-        </p>
-      </div>
-    </div>
-    <div class="flex items-center justify-center gap-4 mb-12">
-      <div>
-        <Link linkTo="/signup" theme="green">Sign Up</Link>
-      </div>
-      <div>
-        <Link linkTo="/signin" theme="default">Sign In</Link>
-      </div>
-    </div>
-    <!-- add icons over image -->
-    <div class="max-w-4xl mx-auto">
-      <img src="/image_colosseum.png" alt="" />
-    </div>
-  </div>
+	<!-- TODO: add bg image -->
+	<div
+		class="font-tasa-orbiter-display text-center px-6 pt-14 md:pt-32 bg-grid bg-no-repeat bg-contain bg-top md:bg-[50%_-18rem]"
+	>
+		<div class="">
+			<h1 class="uppercase font-bold text-6xl md:text-9xl mb-10 md:mb-2">Enter the Arena</h1>
+			<div class="max-w-[938px] mx-auto text-lg mb-10">
+				<p>
+					Claim your place in the next generation of online hackathons.<br
+						class="hidden sm:block"
+					/>Join the most innovative builders in crypto, and launch an industry-defining company on
+					Solana.<br class="hidden sm:block" />Win and be invited to Colosseum’s accelerator, where
+					you’ll receive mentorship from successful ecosystem founders, and grow your start-up with
+					funding from Colosseum and other top investors.
+				</p>
+			</div>
+		</div>
+		<div class="flex items-center justify-center gap-4 mb-12">
+			<div>
+				<Link linkTo="/signup" theme="green">Sign Up</Link>
+			</div>
+			<div>
+				<Link linkTo="/signin" theme="default">Sign In</Link>
+			</div>
+		</div>
+		<!-- add icons over image -->
+		<div class="max-w-4xl mx-auto">
+			<img src="/image_colosseum.png" alt="" />
+		</div>
+	</div>
 </section>
